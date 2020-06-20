3cd7e58abd1e03a13bdce87f62ba9a175734f80f
Dreamwalker <aristojeff@gmail.com>
2020-06-20 23:08:46 +0900

    Update MyApp.kt

--- app/src/main/java/com/dreamwalker/jetpack_compose_101/common/MyApp.kt
+++ app/src/main/java/com/dreamwalker/jetpack_compose_101/common/MyApp.kt
@@ -1,11 +1,14 @@
 package com.dreamwalker.jetpack_compose_101.common
 
 import androidx.compose.Composable
+import androidx.compose.state
 import androidx.ui.core.Modifier
 import androidx.ui.foundation.Text
 import androidx.ui.graphics.Color
 import androidx.ui.layout.Column
+import androidx.ui.layout.height
 import androidx.ui.layout.padding
+import androidx.ui.material.Button
 import androidx.ui.material.Divider
 import androidx.ui.material.MaterialTheme
 import androidx.ui.material.Surface
@@ -24,12 +27,21 @@ fun Greeting(name: String) {
 }
 
 @Composable
-fun MyScreenContent(names:List<String> = listOf("Android", "Dreamwalker")) {
+fun MyScreenContent(names: List<String> = listOf("Android", "Dreamwalker")) {
     Column {
-        for(name in names){
+        for (name in names) {
             Greeting(name = name)
             Divider(color = Color.Black, thickness = 2.dp)
         }
+        Divider(color = Color.Yellow, modifier = Modifier.height(32.dp))
+        Counter()
     }
 }
 
+@Composable
+fun Counter() {
+    val count = state { 0 }
+    Button(onClick = { count.value++ }) {
+        Text("I've been clicked ${count.value} times")
+    }
+}
